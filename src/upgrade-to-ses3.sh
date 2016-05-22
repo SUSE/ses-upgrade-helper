70ef5cf35018d38d7b07000b2d2fd47308fd45f4
Nathan Cutler <ncutler@suse.com>
2016-05-22 09:00:05 +0200

    Revamp preflight check logic
    Simplify the preflight check functions - they only need to return true or
    false.
    
    Run all the preflights; set a flag if any fail. Once all have run, check
    for the flag and exit if it's set.
    
    Signed-off-by: Nathan Cutler <ncutler@suse.com>

--- src/upgrade-to-ses3.sh
+++ src/upgrade-to-ses3.sh
@@ -157,6 +157,23 @@ get_permission () {
     done
 }
 
+run_preflight_check () {
+    local expected_arg_num=2
+    # assert that we have $expected_arg_num arguments - no more, no less!
+    [[ "$#" != "$expected_arg_num" ]] && out_err "$FUNCNAME: Invalid number of arguments. Please provide ${expected_arg_num}.\n" && abort
+
+    local func=$1
+    shift
+    local desc=$1
+    shift
+
+    out_debug "DEBUG: about to run pre-flight check ${func}()"
+    out_white "${desc}\n"
+    out_white "\n"
+
+    "$func" "$@"
+}
+
 # Wrapper to query user whether they really want to run a particular upgrade
 # function.
 run_upgrade_func () {
@@ -219,31 +236,19 @@ run_upgrade_func () {
 # Global pre-flight functions.
 # ------------------------------------------------------------------------------
 running_as_root () {
-    # Script needs to be run as root.
-    if [ "$EUID" -ne 0 ]
-    then
-	out_err "Please run this script as root.\n"
-	return "$failure"
-    fi
-
-    return "$success"
+    test "$EUID" -eq 0
 }
 
-# Returns $success if user ceph is not being used to run any programs, otherwise
-# $failure. In SES2, the user "ceph" was used by ceph-deploy. In SES3, all ceph
-# daemons run as user "ceph", so we need to perform a rename later on.
 user_ceph_not_in_use () {
-    ps -u ceph &>/dev/null
-    # $? == $success implies that at least one process is being run as ceph. Thus,
-    # it is actually the $failure case.
-    [[ "$?" = "$success" ]] && return "$failure" || return "$success"
+    ! ps -u ceph &>/dev/null
 }
 
 preflight_check_funcs+=("running_as_root")
 preflight_check_descs+=(
 "Checking if script is running as root
 =====================================
-su/sudo are just fine."
+The upgrade script must run as root (su/sudo are fine as long as no user
+\"ceph\" is involved)."
 )
 preflight_check_funcs+=("user_ceph_not_in_use")
 preflight_check_descs+=(
@@ -537,23 +542,26 @@ done
 
 out_green "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n"
 out_green "===== SES2.X to SES3 Upgrade =====\n"
-out_green "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n\n"
+out_green "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n"
+out_green "\n"
+out_green "Running pre-flight checks...\n"
+out_green "\n"
 
-out_green "Running Pre-flight Checks...\n"
-out_green "============================\n"
-
-# A small caveat to interactive mode, our global pre-flights need to run in
-# non-interactive mode.
-saved_interactive_mode="$interactive"
-interactive=false
+preflight_failures=false
 for i in "${!preflight_check_funcs[@]}"
 do
-    run_func "${preflight_check_funcs[$i]}" "${preflight_check_descs[$i]}" "$i" false
+    if run_preflight_check "${preflight_check_funcs[$i]}" "${preflight_check_descs[$i]}"
+    then
+        out_green "PASSED\n\n"
+    else
+        out_red "FAILED\n\n"
+        preflight_failures=true
+    fi
 done
-interactive="$saved_interactive_mode"
+[[ "$preflight_failures" = true ]] && out_white "One or more pre-flight checks failed\n" && exit 255
 
-out_green "\nPre-flight Checks Succeeded!\n"
-out_green "============================\n"
+out_green "\n"
+out_green "\nRunning upgrade functions...\n"
 
 for i in "${!upgrade_funcs[@]}"
 do
